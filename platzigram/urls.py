d92beac63e8080c3c44a1be1cb15f841bf7730ab
Reddy 2080 <aaabeeelooon@gmail.com>
2019-09-23 12:42:00 -0400

    hello path added datetime

---
+++ platzigram/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from platzigram import views
+
+urlpatterns = [
+    path('hello', views.hello)
+]
---
+++ platzigram/views.py
@@ -0,0 +1,8 @@
+#django
+from django.http import HttpResponse
+#utilities
+from datetime import datetime
+
+def hello(request):
+    now = datetime.now().strftime('%b %dth')
+    return HttpResponse('Hello, it\'s: {}'.format(now))
